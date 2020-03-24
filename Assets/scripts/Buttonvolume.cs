39addc4f8af9eac5f54fb22e5e989835fd36e235
Arduino <MihailSkubak>
2020-03-24 23:39:43 +0300

    first commit

---
+++ Assets/scripts/Buttonvolume.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Buttonvolume : MonoBehaviour {
+		public Sprite pic1;
+		public Sprite pic2;
+	static bool take;
+
+		private Image im;
+		void Start()
+		{
+		//take = false;
+			im = GetComponent <Image>();
+			//im.sprite = pic1;
+		if (take==true) {
+			im.sprite = pic2;
+			return;
+		}
+		if (take==false) {
+		im.sprite = pic1;
+		return;
+	}
+		}
+	public void Swap()
+		{
+			if (im.sprite == pic1 && !take)
+			{
+				im.sprite = pic2;
+			AudioListener.volume = 0;
+			take = true;
+				return;
+			}
+			if (im.sprite == pic2 && take)
+			{
+				im.sprite = pic1;
+			AudioListener.volume = 1;
+			take = false;
+				return;
+			}
+		}
+	}
